dff84552ad03fe6be996a2dcc36480dcb8eb6968
Ilya <hacharoky86@gmail.com>
2020-06-19 17:01:49 +0300

    Final version first uploading

--- static/home/main.js
+++ static/home/main.js
@@ -1,17 +1,16 @@
-const ru=document.getElementById('RU');
-const en=document.getElementById('EN');
-const blr=document.getElementById('BLR');
+const nav=document.getElementById('nav');
+const menu=document.getElementById('nav_menu');
 
-ru.classList.add('change-language__active');
-const addLine=(event)=>{
+const logoMenu = document.querySelectorAll('.header_icon');
 
-    ru.classList.remove("change-language__active");
-    en.classList.remove("change-language__active");
-    blr.classList.remove("change-language__active");
 
-    event.target.classList.toggle("change-language__active");
-};
-
-ru.addEventListener('click',addLine);
-en.addEventListener('click',addLine);
-blr.addEventListener('click',addLine);
+menu.addEventListener('click',(event)=>{
+    event.preventDefault();
+    nav.classList.toggle('nav_active');
+    logoMenu.forEach(item=>item.classList.remove('header_active'))
+    if(nav.classList.contains('nav_active')){
+        logoMenu[1].classList.add('header_active')
+    }else{
+        logoMenu[0].classList.add('header_active')
+    }
+})
