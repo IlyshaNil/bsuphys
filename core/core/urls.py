c52c2cf4b9adc6f5b7c13adf6fdd99e92a1fe86d
Ilya <hacharoky86@gmail.com>
2020-06-27 00:26:40 +0300

    Template news update

--- core/core/urls.py
+++ core/core/urls.py
@@ -19,15 +19,14 @@ from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path("", include("home.urls")),
     path("admin/", admin.site.urls),
     path("images/", include("images.urls", namespace="images")),
     path("blog/", include("blog.urls", namespace="blog")),
-    path("template/", TemplateView.as_view(template_name='../home/templates/template1.html'),
-         name='about'),
+
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- core/home/views.py
+++ core/home/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.utils import timezone
 
 def index(request):
     return render(request, "index.html")
 
 
 def templates(request):
-    return render(request, "template1.html")
+    posts = Post.objects.latest('publish')
+    return render(request, "template1.html", {"posts": posts})
