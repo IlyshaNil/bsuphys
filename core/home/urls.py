9768fbb8ab48f69a2d6a3745db51a444473a084c
Ilya <hacharoky86@gmail.com>
2020-07-03 21:16:42 +0300

    styles

--- core/home/urls.py
+++ core/home/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [path("", views.index, name="index"),
                path("template/", views.templates, name="index"),
                 path("dekanat/", views.dekanat, name="index"),
-                path("spec/", views.spec),
+                path("spec/", views.spec, name="spec"),
                ]
