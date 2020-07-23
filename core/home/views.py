c300232bb45f84e843805e6ed35cc830d696bca7
Ilya <hacharoky86@gmail.com>
2020-07-23 18:44:15 +0300

    response lang

--- core/home/views.py
+++ core/home/views.py
@@ -7,12 +7,12 @@ from django.conf import settings
 from django.http import HttpResponse
 from django.utils import translation
 
-def language_switch_en():
+def language_switch_en(request):
     user_language = 'en'
     translation.activate(user_language)
     response = HttpResponse()
     response.set_cookie(settings.LANGUAGE_COOKIE_NAME, user_language)
-    return(response)
+    return response
 
 
 def index(request):
