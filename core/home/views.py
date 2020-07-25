e7843e8d9d532860528605b5a7e2a15582969eca
Ilya <hacharoky86@gmail.com>
2020-07-25 18:10:57 +0300

    translate bug fix

--- core/home/views.py
+++ core/home/views.py
@@ -15,7 +15,9 @@ def language_switch_en(request):
     response.set_cookie(settings.LANGUAGE_COOKIE_NAME, user_language)
 
     refresh_path = request.path[:request.path.find("/language_en")]
-    refresh_path = refresh_path.replace('ru', 'en')
+
+
+    refresh_path = refresh_path.replace('/ru/', '/en/')
     response['Location'] = refresh_path
     return response
 
@@ -26,7 +28,7 @@ def language_switch_ru(request):
     response.set_cookie(settings.LANGUAGE_COOKIE_NAME, user_language)
 
     refresh_path = request.path[:request.path.find("/language_ru")]
-    refresh_path = refresh_path.replace('en', 'ru')
+    refresh_path = refresh_path.replace('/en/', '/ru/')
     response['Location'] = refresh_path
     return response
 
