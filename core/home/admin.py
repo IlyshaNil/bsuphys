5a671ed921a8ff5e023cf8246b984b6e17a312db
Ilya <hacharoky86@gmail.com>
2020-07-25 20:44:58 +0300

    Цифры

--- core/home/admin.py
+++ core/home/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import MainPageStatisticNumber
 # Register your models here.
+
+
+@admin.register(MainPageStatisticNumber)
+class MainPageStaticNumbersAdmin(MainPageStatisticNumber):
+    list_display = ["number", "description"]
+    list_filter = ["number", "description"]
+
+    class Meta:
+        verbose_name_plural = "Физический факультет в цифрах"
--- core/home/models.py
+++ core/home/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class MainPageStatisticNumber(models.Model):
+    number = models.IntegerField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
