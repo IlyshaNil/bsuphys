feb4d519ff9351a91d11ce2dfb21643d43272ecf
Ilya <hacharoky86@gmail.com>
2020-07-30 18:33:28 +0300

    force lang 3

--- core/core/settings.py
+++ core/core/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "core.middleware.force_default_language_middleware",
     "django.middleware.locale.LocaleMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
@@ -63,32 +64,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-def force_default_language_middleware(get_response):
-    """
-        Ignore Accept-Language HTTP headers
 
-        This will force the I18N machinery to always choose settings.LANGUAGE_CODE
-        as the default initial language, unless another one is set via sessions or cookies
-
-        Should be installed *before* any middleware that checks request.META['HTTP_ACCEPT_LANGUAGE'],
-        namely django.middleware.locale.LocaleMiddleware
-        """
-    # One-time configuration and initialization.
-
-    def middleware(request):
-        # Code to be executed for each request before
-        # the view (and later middleware) are called.
-        if 'HTTP_ACCEPT_LANGUAGE' in request.META:
-            del request.META['HTTP_ACCEPT_LANGUAGE']
-
-        response = get_response(request)
-
-        # Code to be executed for each request/response after
-        # the view is called.
-
-        return response
-
-    return middleware
 
 
 
