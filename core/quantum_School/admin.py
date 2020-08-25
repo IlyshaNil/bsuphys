92d5db5538007cf680e63a63e508ee4a954489fc
Ilya <hacharoky86@gmail.com>
2020-08-25 19:16:21 +0300

    backend for quantumschool

--- core/quantum_School/admin.py
+++ core/quantum_School/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Advertisement
+from .models import QuantumCourse
+from modeltranslation.admin import TranslationAdmin
 # Register your models here.
+
+
+@admin.register(Advertisement)
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ("title", "body", "publish")
+
+@admin.register(QuantumCourse)
+class QuantumCourseAdmin(admin.ModelAdmin):
+    list_display = ("name",)
--- core/quantum_School/models.py
+++ core/quantum_School/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+from taggit.managers import TaggableManager
+from django.core.files.storage import FileSystemStorage
+from ckeditor.fields import RichTextField
 
-# Create your models here.
+class Advertisement(models.Model):
+    title = models.TextField(null=True, blank=True)
+    body = models.TextField(null=True, blank=True)
+    publish = models.DateTimeField(default=timezone.now)
+
+
+
+class QuantumCourse(models.Model):
+    name = models.TextField(null=True, blank=True)
+    COURCE_CHOICES = (
+        ("1", "Квант-олимпиец"),
+        ("2", "Квант XI"),
+        ("3", "Квант X"),
+        ("4", "Квант IX"),
+        ("5", "Квант-юни"),
+    )
+    description = models.TextField(null=True, blank=True)
+    timeTable = models.ImageField(upload_to="media", null=True, blank=True)
+    lectionsUrl = models.URLField(null=True, blank=True)
+    consultationUrl = models.URLField(null=True, blank=True)
+    taskUrl = models.URLField(null=True, blank=True)
+    course = models.TextField(
+        max_length=10, choices=COURCE_CHOICES, null=True, blank=True, help_text="Обязательное поле!"
+    )
+    advertisement = models.ManyToManyField(Advertisement)
+
+
+
+
