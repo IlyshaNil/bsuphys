130e11a625ed3e9781377bc6a06411aa6f89d1f3
Ilya <hacharoky86@gmail.com>
2020-08-27 09:42:45 +0300

    Update views.py

--- core/quantum_School/views.py
+++ core/quantum_School/views.py
@@ -10,13 +10,13 @@ def quantumUni(request):
     return render(request, "quantumSchoolUni.html")
 
 def quantumNine(request):
-    return render(request)
+    return render(request, "quantumSchool9.html")
 
 def quantumTen(request):
-    return render(request)
+    return render(request, "quantumSchool10.html")
 
 def quantumEleven(request):
-    return render(request)
+    return render(request, "quantumSchool11.html")
 
 def quantumOlimp(request):
     return render(request, "quantumSchoolOlimp.html")
