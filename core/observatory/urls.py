0c6df2bf0bfa2d4c689d1c77db9b641c0e2bd3f1
Ilya <hacharoky86@gmail.com>
2020-10-23 11:26:40 +0300

    Update urls.py

--- core/observatory/urls.py
+++ core/observatory/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [path("", views.observatoryMain, name="observatoryMain"),
                path("employee/", views.observatoryEmployee, name="observatoryMain"),
-               path("equipment/", views.observatoryEmployee, name="observatoryMain"),
+               path("equipment/", views.observatoryEquipment, name="observatoryMain"),
 ]
