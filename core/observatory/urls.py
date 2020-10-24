e8912a6398885c9eabcfbeebd0eb229d7a222bd1
Ilya <hacharoky86@gmail.com>
2020-10-24 14:43:44 +0300

    Update urls.py

--- core/observatory/urls.py
+++ core/observatory/urls.py
@@ -10,8 +10,8 @@ urlpatterns = [path("", views.observatoryMain, name="observatoryMain"),
                 path("science-pop/lections/", views.observatoryPopularLections, name="observatoryMain"),
 
                path("publications/", views.observatoryPublications, name="observatoryMain"),
-               path("publications/articles", views.observatoryPublicationsArticle, name="observatoryMain"),
-               path("publications/books", views.observatoryPublicationsBooks, name="observatoryMain"),
-               path("publications/conferences", views.observatoryPublicationsConference, name="observatoryMain"),
-               path("publications/encyclopedia", views.observatoryPublicationsWiki, name="observatoryMain"),
+               path("publications/articles/", views.observatoryPublicationsArticle, name="observatoryMain"),
+               path("publications/books/", views.observatoryPublicationsBooks, name="observatoryMain"),
+               path("publications/conferences/", views.observatoryPublicationsConference, name="observatoryMain"),
+               path("publications/encyclopedia/", views.observatoryPublicationsWiki, name="observatoryMain"),
 ]
