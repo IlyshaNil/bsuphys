65ae551f441fbbecc1172582179114196ed65e24
Ilya <57794155+IlyshaNil@users.noreply.github.com>
2021-02-04 20:38:32 +0300

    Update models.py

--- core/home/models.py
+++ core/home/models.py
@@ -10,4 +10,13 @@ class MainPageStatisticNumber(models.Model):
         verbose_name_plural = "Физический факультет в цифрах"
 
 
+class KeyPublications(models.Model):
+    title = models.TextField(null=True, blank=True)
+    authors = models.TextField(null=True, blank=True)
+    journal= models.TextField(null=True, blank=True)
+    publicationUrl = models.URLField(max_length=250)
+    image = models.ImageField(upload_to="media/keypublications", null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "Основные публикации"
         
