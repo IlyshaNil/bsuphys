9450b07b2acdaa20a57deab283026d93c727ae0f
Ilya <hacharoky86@gmail.com>
2021-02-05 19:09:16 +0300

    fix: fix staff model

--- core/staff/models.py
+++ core/staff/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from Uploads.models import Uploads
 from staff.staff_conf import choises
+from ckeditor.fields import RichTextField
 
 
 class Staff_unit(models.Model):
     name = models.TextField()
+
     position = models.TextField(null=True, blank=True)
     position_var = models.TextField(
         max_length=30, choices=choises.POSTION_CHOISES, null=True, blank=True
@@ -14,7 +16,10 @@ class Staff_unit(models.Model):
     )
     slug = models.URLField(max_length=250)
     image = models.ImageField(upload_to="media/staff", null=True, blank=True)
-    description = models.TextField()
+    description = RichTextField(null=True, blank=True)
+    address = models.TextField(null=True, blank=True)
+    email = models.TextField(null=True, blank=True)
+    phone = models.TextField(null=True, blank=True)
     uploads = models.ForeignKey(
         Uploads, null=True, blank=True, on_delete=models.CASCADE
     )
