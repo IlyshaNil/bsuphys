b0bb42526bc496a73170ff58995ae51deade8b1e
Ilya <hacharoky86@gmail.com>
2021-03-23 22:57:48 +0300

    timetable

--- core/departments/models.py
+++ core/departments/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+from taggit.managers import TaggableManager
+from django.core.files.storage import FileSystemStorage
+from ckeditor.fields import RichTextField
+from django.db import models
 
 # Create your models here.
