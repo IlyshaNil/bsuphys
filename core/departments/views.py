58af8b451f5b74eebe6f39357794b612c08e89de
Ilya <hacharoky86@gmail.com>
2021-03-24 20:58:14 +0300

    dep 10

--- core/departments/views.py
+++ core/departments/views.py
@@ -15,6 +15,14 @@ import random
 def departmentList(request):
     return render(request, "DepartmentList.html")
 
+def department_detail(request, department):
+    department = get_object_or_404(
+        Department,
+        slug=department,
+
+    )
+    return render(request, "departmentMain.html", {"dep": department})
+
 
 def department_detail_staff(request, department):
     department = get_object_or_404(
