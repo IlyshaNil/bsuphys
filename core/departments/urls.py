afe912e57c70799af07c5f2c10919a3e1998c0fc
Ilya <hacharoky86@gmail.com>
2021-03-24 16:37:45 +0300

    fix: dep url

--- core/departments/urls.py
+++ core/departments/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "departments"
 urlpatterns = [path("", views.departmentList, name="energy department"),
     path(
-            "/departments/<slug:department>/",
+            "departments/<slug:department>/",
             views.department_detail,
             name="department_detail",
     ),
