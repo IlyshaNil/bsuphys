a3320f2df63c92e1ef2dcf4af1403109834ba36e
Ilya <hacharoky86@gmail.com>
2021-09-09 12:11:04 +0300

    fix: admin filter queryset

--- core/blog/admin.py
+++ core/blog/admin.py
@@ -8,7 +8,7 @@ from django.contrib.admin import SimpleListFilter
 @admin.register(Post)
 class PostAdmin(TranslationAdmin):
     list_display = ("title", "slug", "author", "publish", "status", "admin_photo")
-    list_filter = ("status", "publish")
+    list_filter = (PostFilter,)
     search_fields = ("title", "body")
     prepopulated_fields = {"slug": ("title",)}
     raw_id_fields = ("author",)
@@ -17,22 +17,22 @@ class PostAdmin(TranslationAdmin):
 
 
 
-# @admin.register(NoteInMedia)
-# class NoteInMediaAdmin(TranslationAdmin):
-#     list_display = ("title", "body", "status", "admin_photo")
-#     prepopulated_fields = {"slug": ("title",)}
+@admin.register(NoteInMedia)
+class NoteInMediaAdmin(TranslationAdmin):
+    list_display = ("title", "body", "status", "admin_photo")
+    prepopulated_fields = {"slug": ("title",)}
 
-# class PostFilter(SimpleListFilter):
-#     title = 'Status' # or use _('country') for translated title
-#     parameter_name = 'status'
+class PostFilter(SimpleListFilter):
+    title = 'Status' # or use _('country') for translated title
+    parameter_name = 'status'
 
-#     def lookups(self, request, model_admin):
-#         posts = set([c.status for c in Post.model.objects.all()])
-#         return [(c.id, c.title) for c in countries] + [
-#           ('draft', 'Draft - all')]
+    def lookups(self, request, PostAdmin):
+        posts = set([c.status for c in Post.model.objects.all()])
+        return [(c.id, c.title) for c in countries] + [
+          ('draft', 'Draft - all')]
 
-#     def queryset(self, request, queryset):
-#         if self.value() == 'draft':
-#             return queryset.filter(status='draft')
-#         if self.value():
-#             return queryset.filter(status=self.value())
+    def queryset(self, request, queryset):
+        if self.value() == 'draft':
+            return queryset.filter(status__status='draft')
+        if self.value():
+            return queryset.filter(status__id__exact=self.value())
