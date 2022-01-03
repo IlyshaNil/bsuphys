1799d2fcee6f73fadd17d0c22774c319cbb35a32
Ilya <hacharoky86@gmail.com>
2022-01-03 10:13:28 +0300

    add: mag timetable

--- core/home/urls.py
+++ core/home/urls.py
@@ -35,6 +35,7 @@ urlpatterns = [path("", views.index, name="index"),
                path("ru_main/", views.language_switch_ru_main, name="lang_ru_main"),
 
                path("time/", views.timetable, name="info"),
+               path("time/mag/", views.timetableMag, name="info"),
                path("time-exams/", views.timetableExams, name="info"),
 
 
