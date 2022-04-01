22e1922b857ff00a86ce15bd32baf397ebb745be
Ilysha <hacharoky86@gmail.com>
2022-04-01 12:50:36 +0300

    init: tests

---
+++ core/blog/tests/test_views.py
@@ -0,0 +1,37 @@
+from django.test import RequestFactory
+from django.urls import reverse
+from blog.views import post_detail, post_list, weAreInMedia
+from mixer.backend.django import mixer
+import pytest
+from blog.models import Post
+
+
+@pytest.mark.django_db
+class TestViews:
+
+    def test_post_list(self):
+        path = reverse('blog:post_list') 
+        request = RequestFactory().get(path)
+
+        response = post_list(request)
+        assert response.status_code == 200
+
+    def test_weAreInMedia(self):
+        path = reverse('blog:media_notes') 
+        request = RequestFactory().get(path)
+
+        response = weAreInMedia(request)
+        assert response.status_code == 200
+
+    def test_post_detail(self):
+        post = Post.published.latest('publish')
+        path = reverse('blog:post_detail', kwargs = {
+            'year' : post.year,
+            'mounth' : post.month,
+            'day' : post.day,
+            'slug' : post.slug})
+        request = RequestFactory().get(path)
+
+        response = post_detail(request, post.year, post.month, post.day, post.slug)
+        assert response.status_code == 200
+
--- core/staff/apps.py
+++ core/staff/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class StaffConfig(AppConfig):
     name = "staff"
     verbose_name = "База данных сотрудников"
-    label = "Сотрудников база данных"
+    
