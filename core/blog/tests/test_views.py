d3c362fcb6b7650618710658d7abe68bf5cfa2fc
Ilysha <hacharoky86@gmail.com>
2022-04-03 20:18:07 +0300

    fix: blog view test fix

--- core/blog/tests/test_views.py
+++ core/blog/tests/test_views.py
@@ -2,8 +2,9 @@ from django.test import RequestFactory
 from django.urls import reverse
 from blog.views import post_detail, post_list, weAreInMedia
 from mixer.backend.django import mixer
+from blog.models import Post as modelPost
 import pytest
-from blog.models import Post
+import blog
 
 
 @pytest.mark.django_db
@@ -24,15 +25,14 @@ class TestViews:
         assert response.status_code == 200
 
     def test_post_detail(self):
-        post = Post.published.latest('publish')
         path = reverse('blog:post_detail', kwargs = {
-            'year' : post.year,
-            'mounth' : post.month,
-            'day' : post.day,
-            'slug' : post.slug})
+            'year' : 2020,
+            'month' : 7,
+            'day' : 16,
+            'post' : 'pokori-lunu-vmeste-s-lunar-zebro'})
         request = RequestFactory().get(path)
-
-        response = post_detail(request, post.year, post.month, post.day, post.slug)
+        response = post_detail(request, year='2020', month='7', day='16', post='pokori-lunu-vmeste-s-lunar-zebro')
+        
         assert response.status_code == 200
 
 
--- core/blog/views.py
+++ core/blog/views.py
@@ -7,11 +7,11 @@ from taggit.models import Tag
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(
         Post,
-        slug=post,
         status="published",
         publish__year=year,
         publish__month=month,
         publish__day=day,
+        slug=post,
     )
     return render(request, "../templates/templates/post/pageNews.html", {"post": post})
 
