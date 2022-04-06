d39300497c7c9550c168f038130964f235f72a77
Ilysha <hacharoky86@gmail.com>
2022-04-06 18:18:53 +0300

    fix: flake8

--- core/blog/admin.py
+++ core/blog/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from .models import Post
 from .models import NoteInMedia
 from modeltranslation.admin import TranslationAdmin
-from django.contrib.admin import SimpleListFilter, ModelAdmin
 
 
 @admin.register(Post)
