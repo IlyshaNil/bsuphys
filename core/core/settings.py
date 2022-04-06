26809b10cedd19460d7ee8706d915bae9be565f2
Ilysha <hacharoky86@gmail.com>
2022-04-06 10:32:47 +0300

    fix: dynaconf env update

--- core/core/settings.py
+++ core/core/settings.py
@@ -99,20 +99,10 @@ db_url = _settings.DATABASE_URL
 if _settings.ENV_FOR_DYNACONF == "heroku":
     db_url = os.getenv("DATABASE_URL")
 
-if _settings.ENV_FOR_DYNACONF == "GITHUB_WORKFLOW":
-    db_url = os.getenv("DATABASE_GITHUB_URL")
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    # "default": {
-    #   "ENGINE": "django.db.backends.sqlite3",
-    #  "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    # }
-    "default": dj_database_url.parse(db_url, conn_max_age=600)
-    
-}
+DATABASES = {"default": dj_database_url.parse(db_url, conn_max_age=600)}
 
 
 # Password validation
