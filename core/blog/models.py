ad0af62cee1cb258caa32375dd90da17e58b8bd1
Ilysha <hacharoky86@gmail.com>
2022-05-17 20:40:54 +0300

    fix: verbose names

--- core/blog/models.py
+++ core/blog/models.py
@@ -24,18 +24,18 @@ class Post(models.Model):
         ("draft", "draft"),
         ("published", "published"),
     )
-    title = models.TextField()
-    slug = models.SlugField(max_length=250, unique_for_date="publish")
-    image = models.ImageField(upload_to="media", null=True, blank=True)
+    title = models.TextField(verbose_name = "Заголовок")
+    slug = models.SlugField(max_length=250, unique_for_date="publish", verbose_name = "Слаг")
+    image = models.ImageField(upload_to="media", null=True, blank=True, verbose_name = "Афиша")
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="blog_posts"
+        User, on_delete=models.CASCADE, related_name="blog_posts", verbose_name = "Автор"
     )
-    body = RichTextField()
-    publish = models.DateTimeField(default=timezone.now,)
+    body = RichTextField(verbose_name = "Контент")
+    publish = models.DateTimeField(default=timezone.now, verbose_name = "Дата публикации")
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     uploads = models.ForeignKey(
-        Uploads, null=True, blank=True, on_delete=models.CASCADE
+        Uploads, null=True, blank=True, on_delete=models.CASCADE, verbose_name = "Прикрепленные файлы"
     )
     status = models.TextField(
         max_length=10,
@@ -43,13 +43,15 @@ class Post(models.Model):
         null=True,
         blank=True,
         help_text="Обязательное поле!",
+        verbose_name = "Статус"
     )
 
-    tags = TaggableManager()
+    
 
     class Meta:
         ordering = ("-publish",)
         verbose_name_plural = "Добавление новостей"
+        verbose_name = "новость"
 
     def __str__(self):
         return self.title
@@ -73,22 +75,24 @@ class NoteInMedia(models.Model):
         ("draft", "Черновик"),
         ("published", "Опубликовать"),
     )
-    title = models.TextField()
-    slug = models.SlugField(max_length=250, unique_for_date="publish")
-    image = models.ImageField(upload_to="media", null=True, blank=True)
-    body = RichTextField()
+    title = models.TextField(verbose_name = "Заголовок")
+    slug = models.SlugField(max_length=250, unique_for_date="publish", verbose_name = "Слаг")
+    image = models.ImageField(upload_to="media", null=True, blank=True, verbose_name = "Изображение")
+    body = RichTextField(verbose_name = "Текст")
     status = models.TextField(
         max_length=10,
         choices=STATUS_CHOICES,
         null=True,
         blank=True,
         help_text="Обязательное поле!",
+        verbose_name = "Статус"
     )
-    publish = models.DateTimeField(default=timezone.now,)
+    publish = models.DateTimeField(default=timezone.now, verbose_name = "Дата публикации")
 
     class Meta:
         ordering = ("-publish",)
         verbose_name_plural = "Новые заметки в СМИ"
+        verbose_name = "заметку"
 
     def __str__(self):
         return self.title
--- core/staff/models.py
+++ core/staff/models.py
@@ -6,27 +6,28 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class Staff_unit(models.Model):
-    name = models.TextField()
-    position = models.TextField(null=True, blank=True)
-    academic_rank = models.TextField(null=True, blank=True)
+    name = models.TextField(verbose_name = "Имя")
+    position = models.TextField(null=True, blank=True, verbose_name = "Должность")
+    academic_rank = models.TextField(null=True, blank=True, verbose_name = "Степень")
     position_var = models.TextField(
-        max_length=30, choices=choises.POSTION_CHOISES, null=True, blank=True
+        max_length=30, choices=choises.POSTION_CHOISES, null=True, blank=True, verbose_name = "Степень (знач.)"
     )
     specialization = models.TextField(
-        max_length=30, choices=choises.STATUS_CHOICES, null=True, blank=True
+        max_length=30, choices=choises.STATUS_CHOICES, null=True, blank=True, verbose_name = "Кафедра (знач.)"
     )
-    slug = models.SlugField(max_length=250, unique=True)
-    image = models.ImageField(upload_to="media/staff", null=True, blank=True)
-    description = RichTextField(null=True, blank=True)
-    address = models.TextField(null=True, blank=True)
-    email = models.TextField(null=True, blank=True)
-    phone = models.TextField(null=True, blank=True)
+    slug = models.SlugField(max_length=250, unique=True, verbose_name = "Слаг")
+    image = models.ImageField(upload_to="media/staff", null=True, blank=True, verbose_name = "Фото")
+    description = RichTextField(null=True, blank=True, verbose_name = "Описание")
+    address = models.TextField(null=True, blank=True, verbose_name = "Адрес")
+    email = models.TextField(null=True, blank=True, verbose_name = "E-mail")
+    phone = models.TextField(null=True, blank=True, verbose_name = "Телефон")
     uploads = models.ForeignKey(
-        Uploads, null=True, blank=True, on_delete=models.CASCADE
+        Uploads, null=True, blank=True, on_delete=models.CASCADE, verbose_name = "Связанные файлы"
     )
 
     class Meta:
         verbose_name_plural = "Сотрудники"
+        verbose_name = "сотрудника"
         ordering = ["position_var"]
 
     def __str__(self):
