eee0f085f776c9df333af0295ad77faaefbf60b7
IlyshaNil <hacharoky86@gmail.com>
2022-11-04 12:24:55 +0300

    fix: "youth-orgs" url fix "faculty" subpath

--- core/home/urls.py
+++ core/home/urls.py
@@ -58,5 +58,5 @@ urlpatterns = [
     path("educations/trade-union/", views.tradeUnion, name="trade union"),
     path("developments/", views.development, name="development"),
     path("science-school/", views.scienceSchool, name="science-school"),
-    path("youth-orgs/", views.youthOrganizations, name="youth-organizations")
+    path("faculty/youth-orgs/", views.youthOrganizations, name="youth-organizations")
 ]
