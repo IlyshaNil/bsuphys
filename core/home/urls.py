7fefd4fe90ce3a903be9a32b9e7d5323a1487053
Makar M <makar.m@geomotiv.com>
2023-03-15 17:07:43 +0300

    fix: feedback html

--- core/home/urls.py
+++ core/home/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.index, name="index-main"),
     path("template/", views.templates, name="index"),
     path("dekanat/", views.dekanat, name="index"),
     path("spec/", views.spec, name="specialization"),
