3784c5508e80b494dc1f5c0dffce3e9c10647f4e
Makar M <makar.m@geomotiv.com>
2023-04-28 15:24:28 +0300

    add: django weebhook handler

--- core/home/views.py
+++ core/home/views.py
@@ -13,6 +13,22 @@ from django.shortcuts import redirect
 import pprint
 from django.http import HttpResponse
 import os
+import hashlib
+import hmac
+import hmac
+from hashlib import sha1
+from django.http import HttpResponse, HttpResponseForbidden
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+
+import requests
+from ipaddress import ip_address, ip_network
+
+
+from django.conf import settings
+from django.http import HttpResponse, HttpResponseForbidden, HttpResponseServerError
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.encoding import force_bytes
 
 
 def language_switch_en(request):
@@ -333,6 +349,43 @@ def thirdStage(request):
 
 
 def updateGithub(request):
-    os.system('sudo bash /home/dev/cicd')
+    if ifGithubHook(request) and verifyGithubToken(request):
+        os.system('sudo bash /home/dev/cicd')
     return HttpResponse("Hello")
 
+@require_POST
+@csrf_exempt
+def ifGithubHook(request):
+    # Verify if request came from GitHub
+    forwarded_for = u'{}'.format(request.META.get('HTTP_X_FORWARDED_FOR'))
+    client_ip_address = ip_address(forwarded_for)
+    whitelist = requests.get('https://api.github.com/meta').json()['hooks']
+
+    for valid_ip in whitelist:
+        if client_ip_address in ip_network(valid_ip):
+            break
+    else:
+        return HttpResponseForbidden('Permission denied.')
+
+    return True
+
+
+def verifyGithubToken(request):
+    # Verify if request came from GitHub
+    # ...
+
+    # Verify the request signature
+    header_signature = request.META.get('HTTP_X_HUB_SIGNATURE')
+    if header_signature is None:
+        return HttpResponseForbidden('Permission denied.')
+
+    sha_name, signature = header_signature.split('=')
+    if sha_name != 'sha1':
+        return HttpResponseServerError('Operation not supported.', status=501)
+
+    mac = hmac.new(force_bytes(settings.GITHUB_WEBHOOK_KEY), msg=force_bytes(request.body), digestmod=sha1)
+    if not hmac.compare_digest(force_bytes(mac.hexdigest()), force_bytes(signature)):
+        return HttpResponseForbidden('Permission denied.')
+
+    # If request reached this point we are in a good shape
+    return True
